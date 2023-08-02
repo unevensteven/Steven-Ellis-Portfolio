869b72b69102bcb7892523263c534a4e4e81f3e7
unevensteven <128238323+unevensteven@users.noreply.github.com>
2023-08-02 14:01:13 +0100

    Styling
    Styling addition, on hover the portfolio cards will have a shadow
    Added but not set up javascript for the menu button show / dont show

--- js/main.js
+++ js/main.js
@@ -8,10 +8,20 @@
 // 		}
 // 	});
 
-	const menu_btn = document.querySelector('.mini-menu');
-	const mobile_menu = document.querySelector('.mobile-nav');
+		// const menu_btn = document.querySelector('.mini-menu');
+		// const mobile_menu = document.querySelector('.mobile-nav');
 
-	menu_btn.addEventListener('click', function () {
-		menu_btn.classList.toggle('is-active');
-		mobile_menu.classList.toggle('is-active');
+		// menu_btn.addEventListener('click', function () {
+		// 	menu_btn.classList.toggle('is-active');
+		// 	mobile_menu.classList.toggle('is-active');
+		// });
+
+
+	document.addEventListener('DOMContentLoaded', function() {
+		const menuImage = document.getElementById('menuImage');
+		const menu = document.getElementById('menu');
+		menuImage.addEventListener('click', function() {
+			// Toggle the 'hidden' class on the menu to show/hide it
+			menu.classList.toggle('hidden');
+		});
 	});
