e9b9aa31048d17eb2dc78c303ee8f8f70a2ef85e
unevensteven <128238323+unevensteven@users.noreply.github.com>
2023-08-07 16:01:27 +0100

    Form Validation
    Added some elements to my inputs -
    icons success / error
    Javascript for validation
    Styling did not work - took me a while to work this out, but is working.
    added a media query and an id to the contact form so when the screen is wider and the side nav bar appears  the form is not hidden behind the nav bar.
    
    tailwind cdn was not added this is why the styling was off

--- js/main.js
+++ js/main.js
@@ -32,3 +32,78 @@ myHamburgerButton.addEventListener('click', () => {
 });
 
 // Validate form input (will overide HTML atributes if set - no JS then html will apply)
+
+const validateForm = formSelector =>{
+  const formElement = document.querySelector(formSelector);
+
+    const validationOptions = [
+      {      
+      attribute:'required',
+      isValid: input => input.value.trim() !== '',  //any content? trim removes any whitespaces
+      errorMessage: (input, label) => `${label.textContent} is required`,
+    },
+    {
+      attribute:'minLength',
+      isValid: input => input.value && input.value.length >=parseInt(input.minLength, 10),
+      errorMessage: (input, label) =>`${label.textContent} needs to be at least ${input.minLength} characters`,
+    },
+    {
+      attribute: 'pattern',
+      isValid: input => {
+        const patternRegex = new RegExp(input.pattern);
+        return patternRegex.test(input.value);
+      },
+      errorMessage: (input, label) => `Not a valid ${label.textContent}`,
+    }
+  ]
+
+  const validateSingleFormGroup = formGroup => {
+      const label = formGroup.querySelector('label');
+      const input = formGroup.querySelector('input, textarea'); 
+      const errorContainer = formGroup.querySelector('.error');
+      const errorIcon = formGroup.querySelector('.error-icon');
+      const successIcon = formGroup.querySelector('.success-icon');
+
+      let formGroupError = false;
+      for (const option of validationOptions) {
+        if (input.hasAttribute(option.attribute) && !option.isValid(input)){
+          errorContainer.textContent = option.errorMessage(input, label);
+          input.classList.add('border-red-700');
+          input.classList.remove('border-green-700');
+          successIcon.classList.add('hidden');
+          errorIcon.classList.remove('hidden');
+          formGroupError = true;
+        }
+      }
+      if (!formGroupError){
+        errorContainer.textContent='';
+        input.classList.add('border-green-700');
+        input.classList.remove('border-red-700');
+        successIcon.classList.remove('hidden');
+        errorIcon.classList.add('hidden');
+      }
+    };
+
+  formElement.setAttribute('novalidate', '');
+  
+    Array.from(formElement.elements).forEach(element =>{
+      element.addEventListener('blur', event => {
+        validateSingleFormGroup(event.srcElement.parentElement.parentElement);
+      })
+    })
+
+  formElement.addEventListener('submit', event =>{
+    event.preventDefault();
+    validateAllFormGroups(formElement);
+  });
+
+  const validateAllFormGroups = formToValidate =>{
+    const formGroups = Array.from(formToValidate.querySelectorAll('.formGroup'));
+
+    formGroups.forEach(formGroup => {
+      validateSingleFormGroup(formGroup);
+    });
+  }
+};
+
+validateForm('#registrationForm');
